f695d37c0c4758f1ec90a94cfdcbd65d438fe0e7
pakizheng <pakizheng@tencent.com>
2024-01-14 16:39:58 +0800

    修改 readme

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Button } from 'antd';
 import { Link } from 'react-router-dom';
+import Background from '../../images/home-page-bg.jpg';
+
+const commonStyle = {
+  transition: 'all .2s ease-in',
+};
 
 const Home: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen">
-      <div className="flex items-center">
+      <div className="absolute w-full h-full">
+        <img className="w-full h-full object-cover" src={Background} alt="" />
+      </div>
+      <div className="flex items-center relative z-10">
         <Link to="/sign-up">
-          <Button type="primary" className="mr-16">
-            注册账户
-          </Button>
+          <div
+            className="w-240 relative h-48 text-white text-base font-semibold flex items-center justify-center rounded-8 mr-36 border-white overflow-hidden border-[1px] border-solid hover:border-2 hover:text-lg"
+            style={commonStyle}
+          >
+            <div
+              className="absolute w-full h-full bg-white opacity-0 hover:opacity-20"
+              style={commonStyle}
+            ></div>
+            <div>注册账户</div>
+          </div>
         </Link>
         <Link to="/sign-in">
-          <Button>登录账户</Button>
+          <div
+            className="w-240 h-48 relative text-white text-base font-semibold flex items-center justify-center rounded-8 border-white overflow-hidden border-[1px] border-solid hover:border-2 hover:text-lg"
+            style={commonStyle}
+          >
+            <div
+              className="absolute w-full h-full bg-white opacity-0 hover:opacity-20"
+              style={commonStyle}
+            ></div>
+            <div>登录账户</div>
+          </div>
         </Link>
       </div>
     </div>
